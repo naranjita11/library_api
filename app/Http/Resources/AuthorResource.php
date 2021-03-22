803e3f7487964eded172b926ed9a1635e39264fb
Sam Archie <samuelarchie1@gmail.com>
2021-03-22 16:16:42 +0000

    Author Model, Resource and Controller created and migrated

---
+++ app/Http/Resources/AuthorResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "quotes" => $this->quotes,
+            "image_name" => $this->image_name,
+            "image_path" => $this->image_path,
+        ];
+    }
+
+}
