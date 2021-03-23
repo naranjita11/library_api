bc2c27bae61f021e62cb9c23b12e0791e3b2a174
Sam Archie <samuelarchie1@gmail.com>
2021-03-23 12:34:28 +0000

    Seeder work complete

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
+    use HasFactory;
+    
     protected $fillable = ["name", "image_name", "image_path", "bio", "quotes"];
 
-    public function book()
+    public function books()
     {
         return $this->hasMany(Book::class);
     }
